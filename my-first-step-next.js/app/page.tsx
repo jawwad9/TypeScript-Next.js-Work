fb7a46096a33821b652cc75a94fcb283a5754ff4
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-03 18:28:49 +0500

    complete

--- my-first-step-next.js/app/page.tsx
+++ my-first-step-next.js/app/page.tsx
@@ -5,6 +5,8 @@ import ImageMain from "./image.jpg";
 function App(){
   return (
     <>
+            <h1 className="text-4xl font-bold text-white text-center">Hello World</h1>
+
           <div className="flex flex-col items-center pt-48 min-h-screen bg-gradient-to-r from-gray-500 to-slate-900">
         <h1 className="text-4xl font-bold text-white text-center">Hello World</h1>
         <Image
@@ -14,6 +16,8 @@ function App(){
           className="mt-8 rounded-lg shadow-lg h-auto w-auto"
         />
       </div>
+      <h1 className="text-4xl font-bold text-white text-center">Hello World</h1>
+
     
     </>
   )

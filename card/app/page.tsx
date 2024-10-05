bbc9d5b3fe87d15e34d8eef06d20e6092e7be59e
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-05 20:43:19 +0500

    Home page

--- card/app/page.tsx
+++ card/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>page Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatum aperiam architecto, autem minima molestias.
+      <h1>app</h1>
+    </div>
   )
 }
 

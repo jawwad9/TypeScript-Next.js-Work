509d3f7124ce9af8226c1d9ffb8291ca618ac61a
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-08 20:11:58 +0500

    Home About & Contact page

--- routing/app/page.tsx
+++ routing/app/page.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+        <div>page</div>
+
+        <Link href={'About'}>About</Link>
+        <Link href={'Home'}>Home</Link>
+        <Link href={'Contact'}>Contact</Link>
+
+
+
+    </>
   )
 }
 
